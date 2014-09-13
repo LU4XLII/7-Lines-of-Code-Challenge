eb5f65fee9ad65d800a0623328c4601dd85c3853
MagikCow <maxwilkinsonasd@gmail.com>
2014-09-13 16:09:51 +0100

    Delete timer.py

--- Timer--MagikCow-Entery/timer.py
+++
@@ -1,9 +0,0 @@
-import time
-
-go = input("Type go to start the choosing")
-number = float(input("Please type the amount of time in seconds that you would like to time."))
-
-if go == go:
-  print("Go")
-  time.sleep(number)
-  print("Stop)

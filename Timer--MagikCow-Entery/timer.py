94c2bcaf97d11b4fde682c540e0f85ccacb04a82
MagikCow <maxwilkinsonasd@gmail.com>
2014-09-13 16:03:49 +0100

    Create timer.py

---
+++ Timer--MagikCow-Entery/timer.py
@@ -0,0 +1,9 @@
+import time
+
+go = input("Type go to start the choosing")
+number = float(input("Please type the amount of time in seconds that you would like to time."))
+
+if go == go:
+  print("Go")
+  time.sleep(number)
+  print("Stop)
